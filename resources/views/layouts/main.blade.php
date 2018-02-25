29c5692d07f5d13b91548f1f7271319e321e128d
Walid Benmouffok <walid.benmouffok@gmail.com>
2018-02-25 22:50:51 +0100

    site intermédiaire actualisé

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -10,13 +10,13 @@
         <meta property="og:title"         content="Isoler votre maison pour 1€" />
         <meta property="og:description"   content="Profiter du programme d'isolation à 1€ et économisez jusqu'à 30% sur votre facture de chauffage. Isolations des combles, garages et caves." />
         <meta property="og:locale"        content="fr_FR" />
-        <meta property="og:image"         content="{{ asset('images/image-resaux-sociaux2.png') }}" />
+        <meta property="og:image"         content="{{ asset('images/image-partage-reseaux-sociaux.png') }}" />
 
         <meta name="twitter:card" content="summary">
         <meta name="twitter:site" content="@isolehabitat">
         <meta name="twitter:title" content="Isoler votre maison pour 1€">
         <meta name="twitter:description" content="Profiter du programme d'isolation à 1€ et économisez jusqu'à 30% sur votre facture de chauffage. Isolations des combles, garages et caves.">
-        <meta name="twitter:image" content="{{ asset('images/image-resaux-sociaux2.png') }}">
+        <meta name="twitter:image" content="{{ asset('images/image-partage-reseaux-sociaux.png') }}">
         <meta name="twitter:image:width" content="640">
         <meta name="twitter:image:height" content="360">
 
