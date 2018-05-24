fc4d622806d03eca58ced8a0c67da96abb294809
Walid Benmouffok <walid.benmouffok@gmail.com>
2018-05-24 23:08:14 +0200

    section overlay on page load

--- resources/views/site-web/accueil.blade.php
+++ resources/views/site-web/accueil.blade.php
@@ -845,7 +845,7 @@
 @section('javascript')
     <script>
         (function ($) {
-            /*$(document).ready(function() {
+            $(document).ready(function() {
                     var overlay = document.querySelector("#section-overlay");
                     overlay.style.left = "0px";
                     var body = document.getElementsByTagName('body');
@@ -860,7 +860,7 @@
                     $('#wizard-card-test2').hide();
                 });
 
-            });*/
+            });
         })(jQuery);
 
     </script>
