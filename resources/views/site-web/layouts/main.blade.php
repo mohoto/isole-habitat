a30f690c84c7e8cf8c05bd96127519f9a10a5153
Walid Benmouffok <walid.benmouffok@gmail.com>
2018-12-10 00:23:37 +0100

    changement nom facebook share image

--- resources/views/site-web/layouts/main.blade.php
+++ resources/views/site-web/layouts/main.blade.php
@@ -12,7 +12,7 @@
         <meta property="og:title"         content="Isolez votre maison pour 1€" />
         <meta property="og:description"   content="Profitez du programme d'isolation de votre garage et de vos caves pour 1€ et économisez jusqu'à 20% sur votre facture de chauffage.">
         <meta property="og:locale"        content="fr_FR" />
-        <meta property="og:image"         content="{{ asset('images/image-facebook-share-new.jpg') }}" />
+        <meta property="og:image"         content="{{ asset('images/image-facebook-share-new2.jpg') }}" />
         <meta property="og:image:alt"     content="Programme d'isolation de votre garage et de vos caves pour 1€" />
         <meta property="og:image:width"   content="400" />
         <meta property="og:image:height"  content="300" />
