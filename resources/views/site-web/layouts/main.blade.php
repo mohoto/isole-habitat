771ddff25174885a9ae66fff88f21ae3091e583c
Walid Benmouffok <walid.benmouffok@gmail.com>
2018-12-09 15:11:21 +0100

    rectif image facebook share

--- resources/views/site-web/layouts/main.blade.php
+++ resources/views/site-web/layouts/main.blade.php
@@ -12,7 +12,7 @@
         <meta property="og:title"         content="Isolez votre maison pour 1€" />
         <meta property="og:description"   content="Profitez du programme d'isolation de votre garage et de vos caves pour 1€ et économisez jusqu'à 20% sur votre facture de chauffage.">
         <meta property="og:locale"        content="fr_FR" />
-        <meta property="og:image"         content="{{ asset('images/image-facebook-share.jpg') }}" />
+        <meta property="og:image"         content="{{ asset('images/image-facebook-share2.jpg') }}" />
         <meta property="og:image:width" content="400" />
         <meta property="og:image:height" content="300" />
 
