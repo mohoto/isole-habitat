6a2277a63ededea2ef7f419f4f39ca8730295a1d
Walid Benmouffok <walid.benmouffok@gmail.com>
2018-12-12 00:46:26 +0100

    rectif formulaire type iso

--- resources/views/site-web/layouts/formulaire-overlay-sol.blade.php
+++ resources/views/site-web/layouts/formulaire-overlay-sol.blade.php
@@ -181,7 +181,7 @@
                                                 <div class="card card-checkboxes card-hover-effect">
                                                     <div class="icon">
                                                         <i class="icon-fantastic icon-isolation-garage-form"></i>
-                                                        <h6>Garages <br/> en Rdc / sous-sol</h6>
+                                                        <h6>Garages <br/> en Rdc/sous-sol</h6>
                                                     </div>
                                                 </div>
                                             </div>
